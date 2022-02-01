f45369b23a57ac3e7dc37cc1df6f1e8ca32a012d
Juan I. Casareski <juani.casareski@gmail.com>
2022-02-01 20:25:30 -0300

    Feat: Change format for response

--- backend/main.py
+++ backend/main.py
@@ -8,5 +8,7 @@ app = FastAPI()
 
 @app.get("/getCarName/byPlate/{carPlate}")
 async def getCarName(carPlate: str,  q: Optional[str] = None):
-    carModel = await getCarModel(carPlate)
-    return {"carModel": carModel}
+    query = await getCarModel(carPlate)
+    if query is not None:
+        return {"carManufacturer": query[0], "carModel": query[1]}
+    return query
